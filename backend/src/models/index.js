85a96d3dfc8bd59d8e5f6c7bd92e14400254d885
Jeremias <jeremias.elian@gmail.com>
2026-06-16 08:20:37 -0300

    Se elimino Detallepedido

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,37 +1,30 @@
-const sequelize = require('../db');
+const sequelize = require("../db");
 
-const Rol = require('./Rol');
-const Usuario = require('./Usuario');
-const Categoria = require('./Categoria');
-const Producto = require('./Producto');
-const Mesa = require('./Mesa');
-const SesionMesa = require('./SesionMesa');
-const Pedido = require('./Pedido');
-const DetallePedido = require('./DetallePedido');
+const Rol = require("./Rol");
+const Usuario = require("./Usuario");
+const Categoria = require("./Categoria");
+const Producto = require("./Producto");
+const Mesa = require("./Mesa");
+const SesionMesa = require("./SesionMesa");
+const Pedido = require("./Pedido");
 
-Rol.hasMany(Usuario, { foreignKey: 'rolId' });
-Usuario.belongsTo(Rol, { foreignKey: 'rolId' });
+Rol.hasMany(Usuario, { foreignKey: "rolId" });
+Usuario.belongsTo(Rol, { foreignKey: "rolId" });
 
-Categoria.hasMany(Producto, { foreignKey: 'categoriaId' });
-Producto.belongsTo(Categoria, { foreignKey: 'categoriaId' });
+Categoria.hasMany(Producto, { foreignKey: "categoriaId" });
+Producto.belongsTo(Categoria, { foreignKey: "categoriaId" });
 
-Mesa.hasMany(SesionMesa, { foreignKey: 'mesaId' });
-SesionMesa.belongsTo(Mesa, { foreignKey: 'mesaId' });
+Mesa.hasMany(SesionMesa, { foreignKey: "mesaId" });
+SesionMesa.belongsTo(Mesa, { foreignKey: "mesaId" });
 
-Usuario.hasMany(SesionMesa, { foreignKey: 'usuarioAperturaId' });
-SesionMesa.belongsTo(Usuario, { foreignKey: 'usuarioAperturaId' });
+Usuario.hasMany(SesionMesa, { foreignKey: "usuarioAperturaId" });
+SesionMesa.belongsTo(Usuario, { foreignKey: "usuarioAperturaId" });
 
-SesionMesa.hasMany(Pedido, { foreignKey: 'sesionMesaId' });
-Pedido.belongsTo(SesionMesa, { foreignKey: 'sesionMesaId' });
+SesionMesa.hasMany(Pedido, { foreignKey: "sesionMesaId" });
+Pedido.belongsTo(SesionMesa, { foreignKey: "sesionMesaId" });
 
-Usuario.hasMany(Pedido, { foreignKey: 'usuarioCreadorId' });
-Pedido.belongsTo(Usuario, { foreignKey: 'usuarioCreadorId' });
-
-Pedido.hasMany(DetallePedido, { foreignKey: 'pedidoId' });
-DetallePedido.belongsTo(Pedido, { foreignKey: 'pedidoId' });
-
-Producto.hasMany(DetallePedido, { foreignKey: 'productoId' });
-DetallePedido.belongsTo(Producto, { foreignKey: 'productoId' });
+Usuario.hasMany(Pedido, { foreignKey: "usuarioCreadorId" });
+Pedido.belongsTo(Usuario, { foreignKey: "usuarioCreadorId" });
 
 module.exports = {
   sequelize,
@@ -42,5 +35,4 @@ module.exports = {
   Mesa,
   SesionMesa,
   Pedido,
-  DetallePedido
 };
